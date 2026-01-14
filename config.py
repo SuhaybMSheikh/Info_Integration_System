c9b022275f672dcfbc7d2bd86eb49e1d693af684
Viiperlol <suhaybsheikh@hotmail.com>
2026-01-14 16:00:40 +0300

    changed variable name to EXPECTED_ACADEMIC_SESSION, added DEFAULT_BREAK_MINUTES, added DRY_RUN boolean variable

--- config.py
+++ config.py
@@ -4,13 +4,15 @@ DATA_EXCHANGE_ENDPOINT = f"{UNITIME_BASE_URL}/dataExchange"
 USERNAME = "unitime_integration"
 PASSWORD = "password"
 
-ACADEMIC_SESSION = "2025 2025"
+EXPECTED_ACADEMIC_SESSION = "2026 2026"
 
 # Operational hours (used later by time pattern logic)
 DAY_START = "08:30"
 DAY_END = "17:00"
 TIME_STEP_MINUTES = 15
+DEFAULT_BREAK_MINUTES = 15
 
 # Control behavior
 ALLOW_DATE_PATTERN_CREATION = True
 FAIL_ON_MISSING_CURRICULUM = True
+DRY_RUN = False
