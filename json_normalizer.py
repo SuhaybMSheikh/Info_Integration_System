c70e142f4fb9a43d24276e27ad7bf94317c8c070
Viiperlol <suhaybsheikh@hotmail.com>
2026-01-25 16:33:07 +0300

    simplified course_number variable based on JSON data

--- json_normalizer.py
+++ json_normalizer.py
@@ -1,5 +1,5 @@
 def normalize_records(records):
     for r in records:
         r["subject_area"] = r["faculty"]
-        r["course_number"] = r["subject_raw"].split("-")[0]
+        r["course_number"] = r["subject_raw"]
     return records
